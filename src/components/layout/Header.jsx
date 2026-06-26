34e04e5de5c46b3bfa1eb8f9cd6e47fc36f4497e
Seth Dayao <dayao.seth@gmail.com>
2026-06-26 10:34:48 +0800

    chore: initialize file structure

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <header
+      style={{
+        backgroundColor: "#1f2937",
+        color: "white",
+        padding: "1rem 2rem",
+      }}
+    >
+      <h2>Company Logo / Name Here</h2>
+    </header>
+  );
+}
