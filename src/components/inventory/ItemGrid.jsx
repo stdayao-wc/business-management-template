49efc5d9843c66254a88c9c7183363c892c64f8f
Seth Dayao <dayao.seth@gmail.com>
2026-07-07 08:39:39 +0800

    chore: create ItemGrid

---
+++ src/components/inventory/ItemGrid.jsx
@@ -0,0 +1,17 @@
+export default function ItemGrid({
+  children,
+}) {
+  return (
+    <div
+      className="
+        grid
+        gap-6
+        sm:grid-cols-2
+        lg:grid-cols-3
+        xl:grid-cols-4
+      "
+    >
+      {children}
+    </div>
+  );
+}
